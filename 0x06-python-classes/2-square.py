0716db7bdbeba0e68d32726435ab2ca6f024a89e
fred007 <fredjuma8@gmail.com>
2023-01-24 14:26:19 -0800

    correcting task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,26 +1,11 @@
 #!/usr/bin/python3
-"""
-Module 2-square
-Defines class Square with private attribute size and validates size
-"""
+# A class Square that defines a square
 
 
 class Square:
-    """
-    class Square definition
-    Args:
-        size (int): size of a side in square
-    """
-
-    def __init__(self, size=0):
-        """
-        Initializes square
-        Attributes:
-            __size (int): size of a side of square, defaults to 0 if none
-        """
+    def __init__(self, size=0):  # init allow square class to be used
+        self.__size = size   # asign private instance attribute size
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = 
+        if size < 0:
+            raise ValueError("size must be >= 0") 
