0092086d6734b5b24272968244e4f47319091fce
fredjuma007 <fredjuma007>
2023-02-18 00:12:36 +0200

    again

--- 0x07-python-test_driven_development/4-main.py
+++ 0x07-python-test_driven_development/4-main.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module 2-matrix_divided
+Contains method that divides all elements of a matrix and returns new matrix
+Requires same size lists of ints or floats, and max two decimal places
+"""
 print_square = __import__('4-print_square').print_square
 
 print_square(4)
