7107c5a9b2b98e3e03fdcde1b36a2b72ffa44573
fredjuma007 <fredjuma007>
2023-03-13 20:18:24 +0200

    task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -6,5 +6,5 @@ const arg2 = process.argv[3];
 if (arg1 && arg2) {
   console.log(`${arg1} is ${arg2}`);
 } else {
-  console.log("Two arguments are required");
+  console.log('Two arguments are required');
 }
