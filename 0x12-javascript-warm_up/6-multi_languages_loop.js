c69f676a45337a50080ae5abc7f65521949e53c5
fredjuma007 <fredjuma007>
2023-03-13 23:15:36 +0200

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 
-const lines = [
-    'C is fun',
-    'Python is cool',
-    'JavaScript is amazing'
-  ];
-  
-  for (let i = 0; i < lines.length; i++) {
-    console.log(lines[i]);
-  }
+const lines = ['Cis fun', 'Python is cool', 'JavaScript is amazing'];
+for (const l of lines) {
+    console.log(l);
+}
