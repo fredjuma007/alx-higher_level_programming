f8dcc187c532dcecb3f9b57d5178107d6b5c2632
fredjuma007 <fredjuma007>
2023-03-13 22:35:20 +0200

    task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argument = process.argv[2];
+
+const number = parseInt(argument);
+
+if (isNaN(number)) {
+  console.log("Not a number");
+} else {
+  console.log(`My number: ${number}`);
+}
