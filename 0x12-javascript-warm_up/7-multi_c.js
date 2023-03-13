a25f8fcc5ab0d989c36e37d412d362a2c3eb483f
fredjuma007 <fredjuma007>
2023-03-13 23:37:52 +0200

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const x = parseInt(process.argv[2]);
-if (isNaN(x)) {
-  console.log('Missing number of occurrences');
+const num = parseInt(process.argv[2], 10)
+if (isNaN(num)) {
+    console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
-    console.log("C is fun");
-  }
+    for (let i = num; i > 0; i -= 1) {
+        console.log('C is fun');
+    }
 }
