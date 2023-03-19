863aab34dd185a58494fad89ae03540d4fbbefb0
fredjuma007 <fredjuma007>
2023-03-19 22:40:31 +0200

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-# gets all CITIES
+"""
+return cities that are in the state given (tables 'cities' 'states)
+parameters given to script: username, password, database, state
+"""
 
 
-import MySQLdb
-from sys import argv
+def main(args):
+    # gets all CITY stuff
+    if len(args) != 5:
+        raise Exception("need 4 arguments!")
+    db = MySQLdb.connect(host='localhost',
+                         user=args[1],
+                         passwd=args[2],
+                         db=args[3])
+    cur = db.cursor()
+    cur.execute("SELECT c.name FROM cities\
+            c JOIN states s ON s.id=c.state_id\
+            WHERE s.name=%s ORDER BY c.id", (args[4],))
+    states = cur.fetchall()
+    print(", ".join(map(lambda x: "%s" % x, states)))
 
 if __name__ == "__main__":
-
-    # connect to database
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-
-    # create cursor to exec queries using SQL; join two tables for all info
-    cursor = db.cursor()
-    sql_cmd = """SELECT cities.name
-                 FROM states
-                 INNER JOIN cities ON states.id = cities.state_id
-                 WHERE states.name LIKE %s
-                 ORDER BY cities.id ASC"""
-    cursor.execute(sql_cmd, (argv[4], ))
-
-    # format the printing of cities of same state separated by commas
-    print(', '.join(["{:s}".format(row[0]) for row in cursor.fetchall()]))
-
-    cursor.close()
-    db.close()
+    import sys
+    import MySQLdb
+    main(sys.argv)
