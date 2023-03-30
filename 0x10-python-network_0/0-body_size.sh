da5dab7d3ecf9041c8aba4110454f8ac79447176
fred007 <fredjuma8@gmail.com>
2023-03-30 12:50:58 -0700

    well

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#size of content-length
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+# Display size of body of response; Usage: ./0-body_size.sh 0.0.0.0:5000
+curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
 
