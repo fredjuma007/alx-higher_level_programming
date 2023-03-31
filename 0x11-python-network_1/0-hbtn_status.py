c2b2ad58283a12bc174a9e9dea8b5e07cd2e484b
fredjuma007 <fredjuma007>
2023-03-31 19:38:01 +0200

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+    x_request_id = response.headers.get('X-Request-Id')
+    print(f"The value of the X-Request-Id variable is: {x_request_id}")
