36c23ae9c236a34ad53a77e5e96f6f45225a5b95
fredjuma007 <fredjuma007>
2023-04-25 23:18:14 +0200

    all

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
-let file = process.argv[2];
-let fs = require('fs');
-fs.readFile(file, 'utf8', function (err, data = err) {
-  console.log(data);
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
